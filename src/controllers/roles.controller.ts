75aa12d4a81e4e28bf5a4b20e690e681e616d933
Escobar-Rivera <wilalexander05@gmail.com>
2022-10-18 20:05:33 -0600

    creation of files models & routes to rols and users

---
+++ src/controllers/roles.controller.ts
@@ -0,0 +1,11 @@
+import { request, Response } from 'express'
+import { AppDataSource } from '../data-source'
+import { Rol } from '../models/Rol'
+
+class RolesController {
+    static listRoles = async (req: Request, res: Response) => {
+
+    }
+}
+
+export default RolesController 
---
+++ src/routes/index.routes.ts
@@ -0,0 +1,1 @@
+import dontenv from 'dotenv'
